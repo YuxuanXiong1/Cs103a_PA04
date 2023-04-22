edef89a29c733aa21990c74985495b2db0e4fce7
zone-z <zonezhangwx@outlook.com>
2023-04-21 23:57:31 -0400

    1

--- firstapp/routes/tran.js
+++ firstapp/routes/tran.js
@@ -26,7 +26,7 @@ router.get('/tran/',
   async (req, res, next) => {
     if (req.query.sort) {
       const sortKey = req.query.sort;
-      res.locals.items = await TranItem.find({userId:req.user._id}).sort((a, b) => a[sortKey].localeCompare(b[sortKey]));
+      res.locals.items = await TranItem.find({userId:req.user._id});
     } else {
       res.locals.items = await TranItem.find({userId:req.user._id})
     }
